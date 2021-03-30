33da45c9aaf1bd7035ce4282698139d1742b56ae
IvayloNikolov <ivaylonikolov@Ivaylos-MacBook-Air.local>
2021-03-31 02:26:31 +0300

    background fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,17 +4,17 @@ import { Link } from 'gatsby';
 import NotFoundSVG from '../svg/error-404.inline.svg'
 
 function NotFound(){
-    return <>
+    return <div class="background-404">
         <div className="svg-not-found">
             <NotFoundSVG width="250px" height="250px"/>
         </div>
         <h1 className="oops">OOPS!</h1>
-        <p class="not-found-404">404 - PAGE NOT FOUND!</p>
-        <p class="not-found-description">The page you are looking for might have been removed, had its name changed or is temporarily unavilable</p>
+        <p className="not-found-404">404 - PAGE NOT FOUND!</p>
+        <p className="not-found-description">The page you are looking for might have been removed, had its name changed or is temporarily unavilable</p>
         <div className = 'parent'>
             <Link to="/" className="homepage-button">GO TO HOMEPAGE</Link>
         </div>
-    </>
+    </div>
 }
 
 export default NotFound;
