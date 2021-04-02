0ebd3fd43d4648b2c0e0d321885613b7b10f3351
IvayloNikolov <ivaylonikolov@Ivaylos-MacBook-Air.local>
2021-04-02 03:16:41 +0300

    clean

--- .cache/api-runner-browser-plugins 2.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-google-gtag/gatsby-browser.js'),
-      options: {"plugins":[],"trackingIds":["G-5VN7QKW226g"]},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
