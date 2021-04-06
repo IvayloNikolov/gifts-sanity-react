beeb79805982e9b673fe52670a76a0a7602aa27f
Ivaylo Nikolov <ivnikolov721@gmail.com>
2021-04-07 01:15:18 +0300

    slash after contacts

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -9,7 +9,7 @@ function Navigation(){
                     <Link  to="/">Home</Link>
                 </li>
                 <li className='pr-3 menu-item'>
-                    <Link  to="/contacts">Contacts</Link>
+                    <Link  to="/contacts/">Contacts</Link>
                 </li>   
             </ul>
         </nav>
